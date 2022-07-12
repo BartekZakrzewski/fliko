a1cacd7f82431432be988fd950d7a240bee81f5f
Bartosz Zakrzewski <bartek.zakrzewski3012@gmail.com>
2022-07-12 14:27:47 +0200

    first commit

---
+++ assets/js/animate.js
@@ -0,0 +1,37 @@
+// const resolutions = document.querySelector('#sol');
+// const blocks = document.querySelector('.fliko-blok-container');
+
+// let isOverRes = false;
+// let isOverBlocks = false;
+
+// resolutions.addEventListener("mousemove", (e) => {
+//     if(screen.width > 991){
+//         isOverRes = true;
+//     }
+// });    
+
+// resolutions.addEventListener("mouseout", (e) => {
+//     if(screen.width > 991){
+//         isOverRes = false;
+//     }
+// });
+
+// blocks.addEventListener("mousemove", (e) => {
+//     if(screen.width > 991){
+//         isOverBlocks = true;
+//     }
+// });    
+
+// blocks.addEventListener("mouseout", (e) => {
+//     if(screen.width > 991){
+//         isOverBlocks = false;
+//     }
+// })
+
+// document.querySelector('#body').addEventListener("mousemove", () => {
+//     if(isOverBlocks || isOverRes){
+//         blocks.style.display = 'flex';
+//     } else {
+//         blocks.style.display = 'none';
+//     }
+// });
