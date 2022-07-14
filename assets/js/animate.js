4b5e56c77fa43d074b5baffac687d9bdb71424fa
Bartosz Zakrzewski <bartek.zakrzewski3012@gmail.com>
2022-07-14 18:17:26 +0200

    New Commit

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -35,3 +35,25 @@
 //         blocks.style.display = 'none';
 //     }
 // });
+
+// const text = document.querySelector('#js-text');
+// const textCL = text.classList;
+// const sc1 = 'col-md-7', sc2 = 'col-md-9';
+ 
+// window.addEventListener('resize', () => {
+//     if(screen.width > 1370 && !textCL.contains(sc1)){
+//         if(textCL.contains(sc2)){
+//             textCL.remove(sc2);
+//             textCL.add(sc1);
+//         } else {
+//             textCL.add(sc1);
+//         }
+//     } else {
+//         if(textCL.contains(sc1)){
+//             textCL.remove(sc1);
+//             textCL.add(sc2);
+//         } else {
+//             textCL.add(sc2);
+//         }
+//     }
+// })
